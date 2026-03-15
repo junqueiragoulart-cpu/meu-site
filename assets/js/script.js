a7cf7b2fb577f53c744a9cf5e78dabcab1065503
junqueiragoulart-cpu <junqueira.goulart@gmail.com>
2026-03-15 15:08:24 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,1 +1,7 @@
 console.log("JavaScript carregado com sucesso!");
+const botao = document.getElementById("botao-teste");
+const mensagem = document.getElementById("mensagem");
+
+botao.addEventListener("click", function() {
+    mensagem.textContent = "Você clicou no botão! JavaScript funcionando!";
+});
