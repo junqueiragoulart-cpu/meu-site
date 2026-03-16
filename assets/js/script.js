b85f3cc04ae78601da34edd0fc817112ce2a60c3
junqueiragoulart-cpu <junqueira.goulart@gmail.com>
2026-03-16 09:48:37 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,23 +1,7 @@
 console.log("JavaScript carregado com sucesso!");
-/*
-const botao = document.getElementById("botao-teste");
-const mensagem = document.getElementById("mensagem");
 
-if (botao && mensagem) {
-    botao.addEventListener("click", function() {
-        mensagem.textContent = "Você clicou no botão! JavaScript funcionando!";
-    });
-}
-*/
 /* APRENDENDO JS */
 const botaoTema = document.getElementById("tema-btn");
-/*
-if (botaoTema) {
-    botaoTema.addEventListener("click", function() {
-        document.body.classList.toggle("dark-mode");
-    });
-}
-*/
 // Trocar o texto automaticamente
 // const botaoTema = document.getElementById("tema-btn");
 
